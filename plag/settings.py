fecc8d134faae741b5e2d02d0e4618b2b536296e
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2013-08-01 03:26:01 +0300

    Changes made to accomodate hosting on Heroku

--- plag/settings.py
+++ plag/settings.py
@@ -1,5 +1,6 @@
 # Django settings for plag project.
 import os 
+# import dj_database_url 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -8,7 +9,19 @@ ADMINS = (
     ('Chiteri, Martin Akolo', 'chiteri@geek.co.ke'),
 )
 
-MANAGERS = ADMINS
+MANAGERS = ADMINS 
+
+##### Extra config settings for hosting on Heroku ######
+
+# DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*'] 
+
+#### End extra Heroku settings ##### 
 
 DATABASES = {
     'default': {
@@ -62,7 +75,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+# STATIC_ROOT = '' #Without Heroku 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # For Heroku hosting 
+STATIC_ROOT = 'staticfiles' # With Heroku
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -73,7 +88,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths. 
-    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'), 
+    # os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'), # On development server  
+    os.path.join(BASE_DIR, 'static'), 
 )
 
 # List of finder classes that know how to find static files in
