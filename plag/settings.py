30920ff63ffbc323df5fe6b74615d4dd003d1c31
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2013-08-10 03:23:24 +0300

    Migrating from the old WSGI environment for Heroku

--- plag/settings.py
+++ plag/settings.py
@@ -1,6 +1,5 @@
 # Django settings for plag project.
 import os 
-# import dj_database_url 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,35 +8,19 @@ ADMINS = (
     ('Chiteri, Martin Akolo', 'chiteri@geek.co.ke'),
 )
 
-MANAGERS = ADMINS 
+MANAGERS = ADMINS
 
-##### Extra config settings for hosting on Heroku ######
-
-# DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*'] 
-
-# Work with a POSTGRES Db on Heroku 
-if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')} 
-
-#### End extra Heroku Db settings ##### 
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'plag/geek.db',                      # Or path to database file if using sqlite3.
-#        # The following settings are not used with sqlite3:
-#        'USER': '',
-#        'PASSWORD': '',
-#        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#        'PORT': '',                      # Set to empty string for default.
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'plag/geek.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -79,9 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-# STATIC_ROOT = '' #Without Heroku 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # For Heroku hosting 
-STATIC_ROOT = 'staticfiles' # With Heroku
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -92,8 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths. 
-    # os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'), # On development server  
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'), 
 )
 
 # List of finder classes that know how to find static files in
--- plag/wsgi.py
+++ plag/wsgi.py
@@ -19,7 +19,7 @@ import os
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application 
-from dj_static import Cling 
+# from dj_static import Cling 
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
@@ -27,7 +27,7 @@ from dj_static import Cling
 # os.environ["DJANGO_SETTINGS_MODULE"] = "plag_real.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plag.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
